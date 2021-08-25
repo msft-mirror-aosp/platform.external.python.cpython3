f330327f637b6ff81579fbce10573707451221d1
Ryan Prichard <rprichard@google.com>
2021-08-24 23:04:47 -0700

    Support building on Big Sur
    The Kokoro big-sur machines only have Xcode 12.5.1, while the
    catalina machines have 12.2 and 12.4.
    
    https://g3doc.corp.google.com/devtools/kokoro/g3doc/development/macos_external_catalina_config.md
    
    https://g3doc.corp.google.com/devtools/kokoro/g3doc/development/macos_external_big_sur_config.md
    
    By building on big-sur, it should be possible to remove the
    0001-Enable-arm64-builds.patch, and we also wouldn't have to
    change the default Xcode.
    
    Test: kokoro presubmit
    Change-Id: Ia8e98d47de7e0c248484d2285fb20c9062b8a546

--- kokoro/kokoro_build.sh
+++ kokoro/kokoro_build.sh
@@ -8,8 +8,17 @@ python_src=$top/external/python/cpython3
 
 if [ "$(uname)" == "Darwin" ]; then
     # http://g3doc/devtools/kokoro/g3doc/userdocs/macos/selecting_xcode
-    sudo xcode-select -s /Applications/Xcode_12.2.app/Contents/Developer
-    export SDKROOT=/Applications/Xcode_12.2.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
+    xcode=
+    if [ -d /Applications/Xcode_12.5.1.app ]; then
+        xcode=/Applications/Xcode_12.5.1.app
+    elif [ -d /Applications/Xcode_12.4.app ]; then
+        xcode=/Applications/Xcode_12.4.app
+    fi
+    if [ -n "$xcode" ]; then
+        sudo xcode-select -s $xcode/Contents/Developer
+        export SDKROOT=$xcode/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
+    fi
+    echo "Selected Xcode: $(xcode-select -p)"
     (cd $python_src; git apply kokoro/0001-Enable-arm64-builds.patch)
 fi
 
