f062e64873be3fcf871ac8fe696933da8fdab06a
Colin Cross <ccross@android.com>
2026-01-28 22:02:44 -0800

    Move libffi build into build.py
    Building libffi for Linux arm64 will require details of the Linux
    arm64 musl sysroot, move the build into build.py to share the logic
    with building python.
    
    Bug: 474214382
    Test: external/python/cpython3/kokoro_build.sh
    Change-Id: Id4d18ac7b0684a22b242dcc2b0c744da7998f90d

--- kokoro/kokoro_build.sh
+++ kokoro/kokoro_build.sh
@@ -16,9 +16,6 @@ if [ "$(uname)" == "Linux" -a "$SKIP_DOCKER" == "" ]; then
     exit $?
 fi
 
-extra_ldflags=
-extra_notices=
-
 if [ "$(uname)" == "Darwin" ]; then
     # The Kokoro big-sur builder has some extra x86-64 libraries installed in
     # /usr/local (using homebrew), which override the MacOS SDK. At least 3
@@ -59,36 +56,13 @@ if [ "$(uname)" == "Darwin" ]; then
         export SDKROOT=$xcode/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
     fi
     echo "Selected Xcode: $(xcode-select -p)"
-elif [ "$(uname)" == "Linux" ]; then
-    # Build libffi.a for use with the _ctypes module.
-    (cd $top/external/libffi && ./autogen.sh)
-    rm -fr $top/out/libffi
-    mkdir -p $top/out/libffi/build
-    pushd $top/out/libffi/build
-    $top/external/libffi/configure \
-        --enable-static --disable-shared --with-pic --disable-docs \
-        --prefix=$top/out/libffi/install
-    make -j$(nproc) install
-    popd
-
-    # cpython's configure script will use pkg-config to set LIBFFI_INCLUDEDIR,
-    # which setup.py reads. It doesn't use pkg-config to add the library search
-    # dir. With no --prefix, libffi.a would install to /usr/local/lib64, which
-    # doesn't work because, even though setup.py links using -lffi, setup.py
-    # first searches for libffi.{a,so} and needs to find it. setup.py searches
-    # in /usr/local/lib and /usr/lib64, but not /usr/local/lib64.
-    #
-    # Use -Wl,--exclude-libs to hide libffi.a symbols in _ctypes.*.so.
-    export PKG_CONFIG_PATH=$top/out/libffi/install/lib/pkgconfig
-    extra_ldflags="$extra_ldflags -L$top/out/libffi/install/lib64 -Wl,--exclude-libs=libffi.a"
-    extra_notices="$extra_notices $top/external/libffi/LICENSE"
 fi
 
 rm -fr $out
 
 python3 --version
 python3 $python_src/kokoro/build.py $python_src $out $out/artifact \
-    "${KOKORO_BUILD_ID:-dev}" "$extra_ldflags" "$extra_notices"
+    "${KOKORO_BUILD_ID:-dev}"
 
 # Verify that some extensions can be loaded.
 $out/install/bin/python3 -c 'import binascii, bz2, ctypes, curses, curses.panel, hashlib, zlib'
