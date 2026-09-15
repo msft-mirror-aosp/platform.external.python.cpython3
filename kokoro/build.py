cf6134fb6ee1db75a51cc9ab221b44fe1b549088
Colin Cross <ccross@android.com>
2026-09-15 10:28:03 -0700

    cpython3: copy libjemalloc5 when linking against musl
    Musl's builtin malloc trades significant performance for hardening,
    affecting clang performance.  The musl config in
    toolchain/ndk-kokoro/build_utils.py  links against libjemalloc5,
    copy it into the output directory.
    
    Bug: 533100825
    Test: manual
    Change-Id: I3e4c0612b9d55cdb91209251ffb8d2a61df92fc9

--- kokoro/build.py
+++ kokoro/build.py
@@ -303,6 +303,7 @@ def copy_extra_libs(host, install_dir) -> List[Path]:
     if host == Host.LinuxArm64:
         os.makedirs(install_dir / 'lib', exist_ok=True)
         shutil.copy(LinuxArm64Musl.LIBC_MUSL, install_dir / 'lib/libc_musl.so')
+        shutil.copy(LinuxArm64Musl.LIBJEMALLOC, install_dir / 'lib/libjemalloc5.so')
         return LinuxArm64Musl.LIBC_MUSL_NOTICES
     return []
 
